e749f1c3b61a7b317b38706d81a2aa16c46f04c2
Uice Lu <uicestone@gmail.com>
2020-03-01 23:38:36 +0800

    build to js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "quant-eth-1",
-      script: "./node_modules/.bin/ts-node",
-      args: "index.ts -d R -l 200 -s 0.05 -o 0.15 -m 6 -f 5",
+      script: "./dist/index.js",
+      args: "-d R -l 200 -s 0.05 -o 0.15 -m 6 -f 5",
       log_date_format: "YYYY-MM-DD HH:mm:ss.SSS (ZZ)",
       log: true,
       env: {
@@ -12,8 +12,8 @@ module.exports = {
     },
     {
       name: "quant-eth-2",
-      script: "./node_modules/.bin/ts-node",
-      args: "index.ts -d B -l 200 -s 0.05 -o 0.15 -m 6 -f 5",
+      script: "./dist/index.js",
+      args: "-d B -l 200 -s 0.05 -o 0.15 -m 6 -f 5",
       log_date_format: "YYYY-MM-DD HH:mm:ss.SSS (ZZ)",
       log: true,
       env: {
@@ -22,8 +22,8 @@ module.exports = {
     },
     {
       name: "quant-eth-3",
-      script: "./node_modules/.bin/ts-node",
-      args: "index.ts -d S -l 200 -s 0.05 -o 0.15 -m 6 -f 5",
+      script: "./dist/index.js",
+      args: "-d S -l 200 -s 0.05 -o 0.15 -m 6 -f 5",
       log_date_format: "YYYY-MM-DD HH:mm:ss.SSS (ZZ)",
       log: true,
       env: {
